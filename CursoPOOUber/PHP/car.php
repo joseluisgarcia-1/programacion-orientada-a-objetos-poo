38303d896b9348ab022ff213ba90c4349a87d376
joseluisgarcia-1 <jlgarcia002@misena.edu.co>
2022-04-25 13:10:55 -0500

    apuntes hasta la clae 37 que era la última

--- CursoPOOUber/PHP/car.php
+++ CursoPOOUber/PHP/car.php
@@ -1,10 +1,12 @@
 <?php
+
 require_once('account.php');
 class Car {
+
     public $id;
     public $license;
     public $driver;
-    public $passenger;
+    protected $passenger; // Es importante que esta propiedad esté marcada como protected
 
     public function __construct($license, $driver){
         $this->license = $license;
@@ -14,5 +16,6 @@ class Car {
     public function printDataCar() {
         echo "Licencia: $this->license Driver: ".$this->driver->name;
     }
+    
 }
 ?>
